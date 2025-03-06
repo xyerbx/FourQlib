38a786a99edc3158f1290acd370131974c3ffafc
xye <xye@roblox.com>
2025-03-05 17:53:19 -0800

    Add files

--- FourQ_64bit_and_portable/Sources.cmake
+++ FourQ_64bit_and_portable/Sources.cmake
@@ -14,9 +14,9 @@ rbx_target_sources(
     kex.c
     schnorrq.c
     table_lookup.h
+    ../random/random.c
     ../random/random.h
-    ../random/random.h
-    ../sha512/sha512.h
+    ../sha512/sha512.c
     ../sha512/sha512.h
 )
 
